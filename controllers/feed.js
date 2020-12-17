1ff0a1431db4f5590430e571ce3f0d23b7419deb
Severin Spoerri <severin.spoerri@outlook.com>
2020-12-17 07:58:53 +0000

    setup for headers

---
+++ controllers/feed.js
@@ -0,0 +1,15 @@
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [{ title: "First Post", content: "This is the first post" }],
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  console.log("post post");
+  res.status(201).json({
+    message: "Post created",
+    post: { id: new Date().toISOString(), title, content },
+  });
+};
