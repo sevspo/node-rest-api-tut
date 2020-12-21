41973e0a67b3aebe79b250052283627fe9fcc904
Severin Spoerri <severin.spoerri@outlook.com>
2020-12-21 22:27:49 +0000

    first routes

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,15 +1,38 @@
+const { validationResult } = require("express-validator/check");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: "First Post", content: "This is the first post" }],
+    posts: [
+      {
+        id: "1",
+        title: "First Post",
+        content: "This is the first post",
+        imageUrl: "images/duck.jpg",
+        creator: { name: "Sevi" },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({ message: "validation failed", errors: errors.array() });
+  }
   const title = req.body.title;
   const content = req.body.content;
-  console.log("post post");
+  console.log("Post created");
   res.status(201).json({
     message: "Post created",
-    post: { id: new Date().toISOString(), title, content },
+    post: {
+      id: new Date().toISOString(),
+      title,
+      content,
+      creator: { name: "Sevi" },
+      createdAt: new Date(),
+    },
   });
 };
