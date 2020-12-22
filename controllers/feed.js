47638aa2a27f386d22675b8c0440835cbdf80b1a
Severin Spoerri <severin.spoerri@outlook.com>
2020-12-22 22:01:42 +0000

    proper get posts method

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,18 +3,40 @@ const { validationResult } = require("express-validator");
 const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        id: "1",
-        title: "First Post",
-        content: "This is the first post",
-        imageUrl: "images/duck.jpg",
-        creator: { name: "Sevi" },
-        createdAt: new Date(),
-      },
-    ],
-  });
+  Post.find()
+    .then((posts) => {
+      if (!posts) {
+        const error = new Error("posts not found");
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({ message: "posts fetched", posts });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("post not found");
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({ message: "post fetched", post });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
 exports.createPost = (req, res, next) => {
